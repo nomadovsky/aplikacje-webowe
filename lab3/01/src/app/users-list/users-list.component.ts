284b07fb259c95a095722c10d58ecf15779d8c82
Wojciech Kudlicki <kudlickiwojciech@gmail.com>
2023-04-20 09:29:16 +0200

    Add lab 3 css generator functionality

---
+++ lab3/01/src/app/users-list/users-list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lab3-users-list',
+  templateUrl: './users-list.component.html',
+  styleUrls: ['./users-list.component.scss']
+})
+export class UsersListComponent {
+
+}
